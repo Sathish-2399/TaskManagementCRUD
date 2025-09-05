e1ad39e1efe625e07d1fd687563094747a1816a5
Sathish-2399 <2312099@nec.edu.in>
2025-09-05 14:49:23 +0530

    CRUD operation

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,30 +4,47 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Task,setTasks] = useState([])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    //CRUD operation to do task Management System
+      <h1>Task Management System and click the button to complete it</h1>
+      <input
+        type="text"
+        placeholder="Add a new task"
+        value={Task}
+        onChange={(e) => setTasks(e.target.value)}
+
+      ></input>
+
+      <button onClick={() => setTasks([...Task, Task])}>Add Task</button>
+
+
+
+      <ul>
+        <h1>Tasks print in array when complete it delete the Task update the task in array</h1>
+        {Task.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => {
+              const newTasks = [...Task];
+              newTasks.splice(index, 1);
+              setTasks(newTasks);
+            }}>Delete</button>
+            <button onClick={() => {
+              const newTask = prompt("Update the task", task);
+              if (newTask) {
+                const newTasks = [...Task];
+                newTasks[index] = newTask;
+                setTasks(newTasks);
+              }
+            }}>Update</button>
+          </li>
+        ))}
+      </ul>
+
     </>
   )
 }
